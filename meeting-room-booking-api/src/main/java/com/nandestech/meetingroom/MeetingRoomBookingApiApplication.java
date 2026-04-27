c22ab66964ec372c9b3b1a83ac91d09a80ea32fb
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2026-04-27 16:38:10 +0700

    initilize project

---
+++ meeting-room-booking-api/src/main/java/com/nandestech/meetingroom/MeetingRoomBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.nandestech.meetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBookingApiApplication.class, args);
+	}
+
+}
---
+++ meeting-room-booking-api/src/test/java/com/nandestech/meetingroom/MeetingRoomBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandestech.meetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
