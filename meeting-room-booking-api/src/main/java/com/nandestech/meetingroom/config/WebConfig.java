ebada41760325a055a283eeb9caaef5bc5c7b4b0
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2026-07-18 13:44:13 +0700

    add allowed origin

--- meeting-room-booking-api/src/main/java/com/nandestech/meetingroom/config/WebConfig.java
+++ meeting-room-booking-api/src/main/java/com/nandestech/meetingroom/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://roombooking.nandes.tech")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
